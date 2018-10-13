1d26654e4dbe12520a9e1bdd9142efdca241c5e1
Sam Tamburri <tamburrisam@gmail.com>
2018-10-13 16:39:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,7 +26,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   end
 end
 
- def valid_phone_number?(phone)	def valid_phone_number?(phone)
+ def valid_phone_number?(phone)
    arr = phone.scan(/[0-9]/)
   if arr[9] == nil
     return false
