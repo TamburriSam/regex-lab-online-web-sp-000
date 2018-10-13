d25c6af3a436f85a33f5517bc2f47b8ae276ecb5
Sam Tamburri <tamburrisam@gmail.com>
2018-10-13 16:36:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,14 +1,22 @@
-def starts_with_a_vowel?(word)
 
+def starts_with_a_vowel?(word)
+# (/[aeiou]/)
+
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
